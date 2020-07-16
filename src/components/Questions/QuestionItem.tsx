9366067e396e1be10dd12a248d1dd112df3f5637
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-16 18:55:04 +0500

    Added basic implementation of quiz

--- src/components/Questions/QuestionItem.tsx
+++ src/components/Questions/QuestionItem.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { SetStateAction, Dispatch } from "react";
 import { Quiz } from "../../models/Quiz";
 
 interface Props {
   quizItem: Quiz;
+  // setStep: Dispatch<SetStateAction<number>>;
+  // currentIndex: number;
 }
 
 export const QuestionItem: React.FC<Props> = ({ quizItem }) => {
-  return <>{quizItem.question}</>;
+  return (
+    <>
+      <h1 dangerouslySetInnerHTML={{ __html: quizItem.question }}></h1>
+      <ul>
+        {quizItem.choices.map((choice) => (
+          <li key={choice}>{choice}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { Quiz } from "../../models/Quiz";
 import { QuestionItem } from "./QuestionItem";
 
 interface Props {}
-
+const TOTAL_QUESTIONS = 10;
 export const Questions: React.FC<Props> = () => {
+  const [step, setStep] = useState(0);
   const quizes = useSelector<RootState, Quiz[]>((state) => state.quiz.quizes);
-
+  console.log(step);
   return (
     <>
-      {quizes.length > 0 &&
-        quizes.map((item) => <QuestionItem quizItem={item} />)}
+      {quizes.length > 0}
+
+      {quizes.length > 0 && quizes?.[step] && (
+        <>
+          <QuestionItem quizItem={quizes[step]} />
+          <button onClick={() => setStep(step + 1)}>Next</button>
+        </>
+      )}
+      {step > TOTAL_QUESTIONS - 1 && <div>You are done</div>}
     </>
   );
 };
